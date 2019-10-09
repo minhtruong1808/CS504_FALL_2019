38c8906c6300b67f8ab635ceb785e2d5f75d36db
Tys13 <35794005+Tys13@users.noreply.github.com>
2019-10-09 14:18:03 -0700

    Submission for Module1

---
+++ IN/HiteishiReddy/Module1/DataConversion.py
@@ -0,0 +1,3 @@
+price = 10
+print('How many beers you wnat?')
+print('your total price is: $' + str( price * int (input ())))
---
+++ IN/HiteishiReddy/Module1/IfControl.py
@@ -0,0 +1,9 @@
+print ('How old are  you?')
+age = int (input())
+
+if age < 22:
+    print ('you are too young to have a drink.')
+elif age >= 80:
+    print ('ok, you will get a free drink')
+else:
+    print('Sure, enjoy your drink')
