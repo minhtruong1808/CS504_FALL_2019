667609813b5c8e0dde791bcee988b9dc4b86db25
zhuiUnity <34669462+zhuiUnity@users.noreply.github.com>
2019-10-13 16:47:53 -0700

    Update Fib.py

--- Fib.py
+++ Fib.py
@@ -1,16 +1,21 @@
 # Function for nth Fibonacci number 
   
-def Fibonacci(n): 
-    if n<0: 
-        print("Incorrect input") 
-    # First Fibonacci number is 0 
-    elif n==1: 
-        return 0
-    # Second Fibonacci number is 1 
-    elif n==2: 
+def Fibonacci(n):
+
+    a = b = c = 1
+    if n==1:
         return 1
-    else: 
-        return Fibonacci(n-1)+Fibonacci(n-2) 
+    if n==2:
+        return 2
+        
+    for i in range(3,n):
+
+        c = a + b
+        a = b
+        b = c
+
+        return c
+  
   
 # Driver Program 
 
