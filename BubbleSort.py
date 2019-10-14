ea7349b6706011f3e32ac938912f22cf123a481c
Minh T Nguyen <43427377+minhtruong1808@users.noreply.github.com>
2019-10-13 18:20:09 -0700

    Minh Nguyen Submission
    I already submitted it on your repository but I'm just gonna do it here too just to be safe

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,30 +1,32 @@
-def bubble_sort(list, selectedOrder): #selectedOrder could be either 'lth' (low-to-high) or 'htl' (high-to-low)
+def bubble_sort(list,order):
     sorted_list = list[:]
     is_sorted = False
+    turns = 0
     while is_sorted == False:
         swaps = 0
-        if (str(selectedOrder) == "lth"): # low-to-high sorting
-            for i in range(len(list) - 1):
-                if sorted_list[i] > sorted_list[i + 1]: # swap
+        if order == "lth":
+            for i in range(len(list) - 1): #loop through the list comparing number with next number
+                if sorted_list[i] > sorted_list[i + 1]: # swap if the number is bigger than the previous number
+                    temp = sorted_list[i] #temporary variable to store the to be swap number
+                    sorted_list[i] = sorted_list[i + 1]
+                    sorted_list[i + 1] = temp
+                    swaps += 1 #print out a number of swap in ONE ITERATE
+                    turns += swaps
+        elif order == "htl":
+            for i in range(len(list) - 1): 
+                if sorted_list[i] < sorted_list[i + 1]:
                     temp = sorted_list[i]
                     sorted_list[i] = sorted_list[i + 1]
                     sorted_list[i + 1] = temp
                     swaps += 1
-                    print(swaps)
-        elif (str(selectedOrder) == "htl"): # high-to-low sorting
-            for i in range(len(list) - 1):
-                if sorted_list[i] < sorted_list[i + 1]: # swap
-                    temp = sorted_list[i]
-                    sorted_list[i] = sorted_list[i + 1]
-                    sorted_list[i + 1] = temp
-                    swaps += 1
-                    print(swaps)
-        else: # for incorrect selectedOrder, user will be prompt with message.
-            print("The second argument is incorrect!")
-            print("Please select correct order. Either 'lth' for low to high sorting or 'htl' for high to low sorting.")
+                    turns += swaps
         if swaps == 0:
             is_sorted = True
+    print(turns)
     return sorted_list
+
+print(bubble_sort([2, 1, 4, 5, 3],"htl"))
+
 
 print(bubble_sort([2, 3, 1], "lth"))  # result: [1, 2, 3] 
 print(bubble_sort([2, 3, 1], "htl"))  # result: [3, 2, 1]
